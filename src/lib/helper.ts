3265dcbec22b20e7e4b84808e291071e912733af
Mukul Sharma <mukulsharma262002@gmail.com>
2025-05-23 11:19:30 +0530

    Added supported regions and default locale in helpers file

---
+++ src/lib/helper.ts
@@ -0,0 +1,23 @@
+export const regions = [
+  "en-AU",
+  "nl-NL",
+  "de-CH",
+  "fr-CH",
+  "en-NZ",
+  "en-ZA",
+  "sv-SE",
+  "nb-NO",
+  "es-ES",
+  "pt-PT",
+  "fr-BE",
+  "nl-BE",
+  "fr-FR",
+  "en-CA",
+  "fr-CA",
+  "es-MX",
+  "pt-BR",
+  "en-SA",
+  "en-AE",
+];
+
+export const defaultLocale = "en-AU";
